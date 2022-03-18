1803780c36f29811d84cc0e41988d05a84d84c81
Nussi1 <59906591+Nussi1@users.noreply.github.com>
2022-03-18 20:35:55 +0600

    Add files via upload

---
+++ whileeven.py
@@ -0,0 +1,5 @@
+for i in range(0,300,2):
+    print(i,end='  ')
+    if i >= 236:
+        break
+print('237 is final number')
---
+++ zadanie3_5.py
@@ -0,0 +1,5 @@
+my_list = [2,4,6,8,10,1,3,5,7,9,11,13,17]
+even = []
+for i in range(0,len(my_list),2):
+    even.append(my_list[i])
+print(even)
